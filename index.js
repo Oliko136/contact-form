7c720078b24dc234fc1f93d609269471760a6afc
Oliko136 <olchick136@gmail.com>
2025-04-30 20:28:56 +0400

    Make error messages targeted

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const form = document.getElementById('form-js');
 const success = document.getElementById('success-js');
-const error = document.querySelectorAll('#error-js');
+const firstNameError = document.getElementById('first-name-error-js');
+const lastNameError = document.getElementById('last-name-error-js');
+const emailError = document.getElementById('email-error-js');
+const queryError = document.getElementById('query-error-js');
+const messageError = document.getElementById('message-error-js');
+const consentError = document.getElementById('consent-error-js');
 
 form.addEventListener('submit', handleSubmit);
 
@@ -18,18 +23,41 @@ function handleSubmit (e) {
 function validate (data) {
     const { "first-name": firstName, "last-name": lastName, email, query, message, consent } = data;
 
-    if (!firstName || !lastName || !email || !query || !message || !consent) {  
-        renderError();
+    if (!firstName) {  
+        renderError(firstNameError);
+        return;
+    }
+
+    if (!lastName) {  
+        renderError(lastNameError);
+        return;
+    }
+
+    if (!email) {  
+        renderError(emailError);
+        return;
+    }
+
+    if (!query) {  
+        renderError(queryError);
+        return;
+    }
+
+    if (!message) {  
+        renderError(messageError);
+        return;
+    }
+
+    if (!consent) {  
+        renderError(consentError);
         return;
     }
 
     renderSuccess();
 }
 
-function renderError() {
-    error.forEach((el) => {
-        el.classList.remove('is-hidden');
-    });
+function renderError(element) {
+    element.classList.remove('is-hidden');
 }
 
 function renderSuccess () {
