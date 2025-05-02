ab9d16a220431d28a83a1d3e88df98322fb4d9cf
Oliko136 <olchick136@gmail.com>
2025-05-02 17:45:03 +0400

    Finish the validation logic

--- index.js
+++ index.js
@@ -3,47 +3,96 @@ const success = document.getElementById('success-js');
 
 form.addEventListener('submit', handleSubmit);
 
-function handleSubmit (e) {
+function handleSubmit(e) {
     e.preventDefault();
 
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData);
-    const elementArr = Object.keys(data);
-
-    elementArr.forEach((element) => {
-        const elementValue = data[element];
-        const elementError = document.getElementById(`${element}-error-js`);
-
-        console.log(elementValue);
-        console.log(elementError);
-        console.log(elementError.classList.contains('is-hidden'));
-
-       validate(elementValue)
-            ? renderSuccess()
-            : renderError(elementError);
+    // Get the form data
+    const data = {
+        'first-name': form.elements['first-name'].value,
+        'last-name': form.elements['last-name'].value,
+        email: form.elements['email'].value,
+        query: form.elements['query'].value,
+        message: form.elements['message'].value,
+        consent: form.elements.consent.checked
     }
-    );
+    const elementArr = Object.entries(data);
+
+    // Check if the form is valid
+    // If the form is valid, render the success message
+    if (validateForm(elementArr)) {
+        renderSuccess();
+    }
 }
 
-function validate(element) {
-    element = element.trim();
+function validateForm(elementArr) {
+    let isValid = false;
+    const keyArr = [];
+    
 
-    if (element === '') {
+    elementArr.forEach((element) => {
+        const [key, value] = element;
+        const error = document.getElementById(`${key}-error-js`);
+
+        switch (key) {
+            case 'first-name':
+                // If the key is first name, check if the value is empty
+                value === '' ? renderError(error) : keyArr.push(key) && clearError(error);
+                break;
+            case 'last-name':
+                // If the key is last name, check if the value is empty
+                value === '' ? renderError(error) : keyArr.push(key) && clearError(error);
+                break;
+            case 'email':
+                // If the key is email, check if the value is a valid email
+                value === '' ? renderError(error) : checkEmail(value) && keyArr.push(key) && clearError(error);
+                break;
+            case 'query':
+                // If the key is query, check if the value is empty
+                value === '' ? renderError(error) : keyArr.push(key) && clearError(error);
+                break;
+            case 'message':
+                // If the key is message, check if the value is empty
+                value === '' ? renderError(error) : keyArr.push(key) && clearError(error) ;
+                break;
+            case 'consent':
+                // If the key is consent, check if the checkbox is checked
+                !value ? renderError(error) : keyArr.push(key) && clearError(error);
+                break;
+            default:
+                renderError(error)
+                break;
+        }
+        
+    })
+    
+    keyArr.length === 6 ? isValid = true : isValid = false;
+
+    return isValid;
+}
+
+function checkEmail(value) {
+    const regexError = document.getElementById('regex-error-js');
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // Check if the email is valid
+    if (!emailRegex.test(value)) {
+        renderError(regexError);
         return false;
+    } else {
+        clearError(regexError);
+        return true;
     }
-
-    return true;
 }
 
 function renderError(element) {
     element.classList.remove('is-hidden');
 }
 
-function clearError (element) {
+function clearError(element) {
     element.classList.add('is-hidden');
 }
 
-function renderSuccess () {
+function renderSuccess() {
     success.classList.remove('is-hidden');
 
     form.reset();
