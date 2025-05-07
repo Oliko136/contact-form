7d97ac42acb28aecf7bae787aa73ee0469a8ebcf
Oliko136 <olchick136@gmail.com>
2025-05-07 18:59:02 +0400

    Fix extra space issue

--- index.js
+++ index.js
@@ -37,27 +37,27 @@ function validateForm(elementArr) {
         switch (key) {
             case 'first-name':
                 // If the key is first name, check if the value is empty
-                value === '' ? renderError(error) : keyArr.push(key) && clearError(error);
+                value === '' ? renderError(error, 'First name is required') : keyArr.push(key) && clearError(error);
                 break;
             case 'last-name':
                 // If the key is last name, check if the value is empty
-                value === '' ? renderError(error) : keyArr.push(key) && clearError(error);
+                value === '' ? renderError(error, 'Last name is required') : keyArr.push(key) && clearError(error);
                 break;
             case 'email':
                 // If the key is email, check if the value is a valid email
-                value === '' ? renderError(error) : checkEmail(value) && keyArr.push(key) && clearError(error);
+                value === '' ? renderError(error, 'Email address is required') : checkEmail(value) && keyArr.push(key) && clearError(error);
                 break;
             case 'query':
                 // If the key is query, check if the value is empty
-                value === '' ? renderError(error) : keyArr.push(key) && clearError(error);
+                value === '' ? renderError(error, 'Please select a query type') : keyArr.push(key) && clearError(error);
                 break;
             case 'message':
                 // If the key is message, check if the value is empty
-                value === '' ? renderError(error) : keyArr.push(key) && clearError(error) ;
+                value === '' ? renderError(error, 'Message is required') : keyArr.push(key) && clearError(error) ;
                 break;
             case 'consent':
                 // If the key is consent, check if the checkbox is checked
-                !value ? renderError(error) : keyArr.push(key) && clearError(error);
+                !value ? renderError(error, 'To submit this form, please consent to being contacted') : keyArr.push(key) && clearError(error);
                 break;
             default:
                 renderError(error)
@@ -77,7 +77,7 @@ function checkEmail(value) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     // Check if the email is valid
     if (!emailRegex.test(value)) {
-        renderError(regexError);
+        renderError(regexError, 'Please enter a valid email address');
         return false;
     } else {
         clearError(regexError);
@@ -85,11 +85,13 @@ function checkEmail(value) {
     }
 }
 
-function renderError(element) {
+function renderError(element, message) {
+    element.textContent = message;
     element.classList.remove('is-hidden');
 }
 
 function clearError(element) {
+    element.textContent = '';
     element.classList.add('is-hidden');
 }
 
@@ -97,11 +99,13 @@ function renderSuccess() {
     const clone = successTmpl.content.cloneNode(true);
     clone.querySelector('.success_title').textContent = 'Message Sent!';
     clone.querySelector('.success_text').textContent = "Thanks for completing the form. We'll be in touch soon!";
+    success.classList.remove('is-hidden');
     success.appendChild(clone);
 
     form.reset();
 
     setTimeout(() => {
         success.classList.add('is-hidden');
+        success.innerHTML = '';
     }, 5000);
 }
